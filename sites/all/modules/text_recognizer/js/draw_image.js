7dced2d98e3c1a41e17039636d4e8181a318aafb
Molly LeCompte <mollyklecompte@gmail.com>
2016-12-20 16:57:56 -0800

    begin text_recognizer module.

---
+++ sites/all/modules/text_recognizer/js/draw_image.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var canvas = document.getElementById("image-canvas");
+    var context = canvas.getContext("2d");
+    var imgdiv = document.getElementbyId("edit-image-upload-upload")
+    var imglink = imgdiv.getElementsByTagName("a");
+    var img =
+    context.drawImage(img,10,10);
+};
